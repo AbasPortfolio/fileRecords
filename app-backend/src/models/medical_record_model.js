67d123f065453941a117dcaf90fae66be6b23ddd
Abas Muzi <106030034+AbasPortfolio@users.noreply.github.com>
2025-11-27 15:37:18 +0200

    Add files via upload

---
+++ app-backend/src/models/medical_record_model.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose';
+
+const MedicalSchema = new mongoose.Schema({
+    patient_name: {
+        type: String,
+        required: true,
+    },
+    patient_gender: {
+        type: Boolean,
+        required: true,
+    },
+    patient_age: {
+        type: Number,
+        required: true,
+    },
+    diagnosis: {
+        type: String,
+        required: true,
+    },
+    doctor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    pathology: {
+        type: String,
+        required: true,
+    },
+    date_recorded: {
+        type: Date,
+        default: Date.now,
+    },
+    patient_triage: {
+        type: [String],
+    },
+    notes: {
+        type: String,
+    },
+    file_number: {
+        type: Number,
+    },
+});
+
+const MedicalRecord = mongoose.model('MedicalRecord', MedicalSchema);
+
+export default MedicalRecord;
